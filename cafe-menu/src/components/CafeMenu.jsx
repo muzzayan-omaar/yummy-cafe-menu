e6cabca98137ea84d91a75dec45cbc9fc5b15996
muzzayan-omaar <muzzayanomaar@gmail.com>
2026-02-05 22:58:26 +0400

    cleaning up 7

--- cafe-menu/src/components/CafeMenu.jsx
+++ cafe-menu/src/components/CafeMenu.jsx
@@ -102,7 +102,7 @@ export default function CafeMenu() {
   }
 
   return (
-    <div className="relative min-h-screen bg-gray-50 dark:bg-[#0b1020] text-gray-900 dark:text-gray-100 overflow-hidden">
+<div className="relative min-h-screen text-gray-900 dark:text-gray-100">
 
 
 
